4a611b782f35262551d26811cf32faf98af87700
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-09-27 15:39:23 -0300

    adicionar projeto guiado da semana 8

---
+++ reprogramaflix/src/controller/seriesController.js
@@ -0,0 +1,29 @@
+const { response } = require("express");
+const series = require("../models/series.json");
+
+const getAll = (request, response) => {
+  console.log(request.url);
+  response.status(200).send(series);
+};
+
+const getById = (request, response) => {
+  const id = request.params.id;
+  response.status(200).send(series.find((serie) => serie.id == id));
+};
+
+const getByGenre = (request, response) => {
+  const genre = request.params.genre;
+  response
+    .status(200)
+    .send(
+      series.filter((serie) =>
+        serie.genre.map((g) => g.toLowerCase()).includes(genre.toLowerCase())
+      )
+    );
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getByGenre,
+};
