12e7ed30cdf608244d0cf76a0b41ed5f75c1ccc2
Denise <deniseevelynk3@hotmail.com>
2020-10-02 22:02:25 -0300

    First commit

---
+++ reprogramaflix/src/controller/filmesController.js
@@ -0,0 +1,38 @@
+const filmes = require("../models/filmes.json");
+
+const getAll = (request, response) => {
+    console.log(request.url);
+    response.status(200).send(filmes);
+}
+
+const getById = (request, response) => {
+    const id = request.params.id;
+    response.status(200).send(filmes.find(filme => filme.id == id ))
+}
+
+const getByTitle =(request, response) => {
+    const titulo = request.query.titulo;
+    response.status(200).send(filmes.find(filme => filme.Title == titulo))
+}
+
+const getByGenre = (request, response) => {
+    const genero = request.query.genero;
+    let novaLista = [];
+//pega a string e transforma num array com split
+    filmes.forEach(filme => {
+        let generoList = filme.Genre.split(",");
+        for(item of generoList) {
+            if(item.includes(genero) && filme.Genre.includes(genero)) {
+                novaLista.push(filme);
+            }
+        }
+    })
+    response.status(200).send(novaLista);
+}
+//exporta as funções gets
+module.exports = {
+    getAll,
+    getById,
+    getByTitle,
+    getByGenre
+}
