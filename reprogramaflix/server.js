a408a9c9b5d2949a0aea88af90ec98aefc6ea446
thaispisani <thaispisani_@hotmail.com>
2020-10-03 21:25:03 -0300

    Adicionando Projeto reprogramaflix

---
+++ reprogramaflix/server.js
@@ -0,0 +1,7 @@
+const app = require('./src/app');
+
+const PORT = 5050;
+
+app.listen(PORT, () => {
+    console.log("TA RODANDO NA PORTA " + PORT);
+});
---
+++ reprogramaflix/src/routes/filmesRoutes.js
@@ -0,0 +1,14 @@
+const controller = require("../controller/filmesController")
+
+const express = require("express")
+const router = express.Router()
+
+router.get("/todos", controller.getAll)
+router.get("/title", controller.getByTitle)
+router.get("/genre", controller.getByGenre)
+router.get("/actors", controller.getByActors)
+router.get("/:id", controller.getById)
+
+
+
+module.exports = router 
