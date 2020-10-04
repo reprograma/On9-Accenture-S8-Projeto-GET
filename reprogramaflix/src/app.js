b40c19f129322fe39f4a7ece8f3ffc373b8babca
Fabi <fabicaneyu@gmail.com>
2020-10-03 23:26:49 -0300

    projeto da semana GET

---
+++ reprogramaflix/src/app.js
@@ -0,0 +1,12 @@
+const { response } = require("express")
+const express = require("express")
+
+const app = express()
+
+const filmes = require("./routes/filmesRoutes")
+const series = require("./routes/seriesRoutes")
+
+app.use("/filmes", filmes)
+app.use("/series", series)
+
+module.exports = app
